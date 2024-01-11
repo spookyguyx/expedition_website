5561af1bdfa91b00821b797d1be2b8e728bfaa31
Anastasia <anship2206@mail.ru>
2024-01-11 22:42:58 +0300

    I connected the files

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'main/main.html'
+class EventsPage(TemplateView):
+    template_name = 'main/events.html'
+class PublicationPage(TemplateView):
+    template_name = 'main/publication.html'
+class Separate_newsPage(TemplateView):
+    template_name = 'main/Separate_news.html'
